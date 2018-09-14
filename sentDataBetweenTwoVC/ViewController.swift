7763da93fc15255853cc7c906d3ff3bf3f551f83
chan wayne <waynechan99999@yahoo.com.hk>
2018-09-14 11:40:45 +0800

    Initial Commit

---
+++ sentDataBetweenTwoVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sentDataBetweenTwoVC
+//
+//  Created by wayne chan on 14/9/2018.
+//  Copyright © 2018 chan siu wayne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
